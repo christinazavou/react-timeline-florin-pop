3bbb88c5a28204689951d0135c5a71b98f7d1f87
Minesh Kothari <mkothari@fdmplc.com>
2019-05-15 15:56:14 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Timeline from './components/Timeline';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    timelineData: [
+      {
+        text: 'Wrote my first blog post ever on Medium',
+        date: 'March 03 2017',
+        category: {
+            tag: 'medium',
+            color: '#018f69'
+        },
+        link: {
+            url:
+                'kotharidigital.com',
+            text: 'Read more'
+        }
+      },
+      {
+        text: 'This is the second timeline item',
+        date: 'September 22 2017',
+        category: {
+            tag: 'tag-two',
+            color: '#018f69'
+        },
+        link: {
+            url:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text: 'Read more'
+        }
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Timeline data={this.state.timelineData} />
+      </div>
+    );
+  }
 }
 
 export default App;
